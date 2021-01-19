f2f810bd0c76b409a5351edaf94925cde6041177
Quizvilla-IWP <77549333+Quizvilla-IWP@users.noreply.github.com>
2021-01-19 10:28:23 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "";
-$username = "root";
-$password = "";
-$dbname = "quiz";
+$servername = "http://sql12.freemysqlhosting.net/";
+$username = "sql12387426";
+$password = "Kgysz8h12M";
+$dbname = "sql12387426";
 $db = new mysqli($servername, $username, $password, $dbname);
 
 // if ($db->connect_error) {
