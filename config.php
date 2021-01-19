1179502ae1373cf5d8af7e01cb238eacd767c850
Quizvilla-IWP <77549333+Quizvilla-IWP@users.noreply.github.com>
2021-01-19 10:19:53 +0530

    Initial Commit

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "";
+$username = "root";
+$password = "";
+$dbname = "quiz";
+$db = new mysqli($servername, $username, $password, $dbname);
+
+// if ($db->connect_error) {
+//     die("connection failed" . $db->connect_error);
+// }
+// echo "connected successfully";
